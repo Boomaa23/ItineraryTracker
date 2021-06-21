957b454d1998d87580ee340aeb9f65a0b69320fd
Boomaa23 <cmo93003@yahoo.com>
2021-06-20 23:15:51 -0700

    Initial commit

---
+++ app/src/main/java/com/boomaa/itinerarytracker/ui/current/CurrentViewModel.kt
@@ -0,0 +1,12 @@
+package com.boomaa.itinerarytracker.ui.current
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CurrentViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is current Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/boomaa/itinerarytracker/ui/saved/SavedViewModel.kt
@@ -0,0 +1,12 @@
+package com.boomaa.itinerarytracker.ui.saved
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SavedViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is saved Fragment"
+    }
+    val text: LiveData<String> = _text
+}
